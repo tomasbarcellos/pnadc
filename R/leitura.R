a9230792890cd14c46be1aa35c6f692d86b6b4e1
Tomas de Siervi Barcellos <tomasbarcellos@gmail.com>
2022-10-31 05:39:17 -0300

    Altera funcao de leitura para incluir instrucao

--- R/leitura.R
+++ R/leitura.R
@@ -16,7 +16,7 @@ ler_pnad <- function(ano, trimestre) {
 
   vars <- c("Ano", "Trimestre", "UF", "V1008", "V1014",
             "V1016", "V1022", "V1027", "V1028", "V1029",
-            "V2007", "V2010", "V2009", "VD4009","VD4010",
+            "V2007", "V2010", "V2009", "V3004", "VD4009","VD4010",
             "V4010", "V4012", "V4013", "VD4017", "V403312",
             "V4039", "V4041", "V4043", "V4044", "V4047",
             "V4048", "V405112", "V4056"
@@ -24,15 +24,14 @@ ler_pnad <- function(ano, trimestre) {
 
   input <- system.file("extdata", "input_PNADC_trimestral.txt",
                        package = "pnadc")
-  # dic <- system.file("extdata", "dicionario_PNADC_microdados_trimestral.xls", package = "pnadc")
 
-  resp <- PNADcIBGE::read_pnadc(path, input, vars) %>%
+  PNADcIBGE::read_pnadc(path, input, vars) %>%
       dplyr::rename(
         ano = Ano, trimestre = Trimestre, uf = UF, estrato = Estrato, upa = UPA,
         num_domicilio = V1008, grupo_amostra = V1014, num_entrevista = V1016,
         situacao_domicilio = V1022, peso = V1027,
         peso_posestrat = V1028, proj_pop = V1029,
-        sexo = V2007, cor = V2010, idade = V2009,
+        sexo = V2007, cor = V2010, idade = V2009, instrucao = V3004,
         profissao_principal = V4010, profissao_secundario = V4041,
         jornada_principal = V4039, jornada_secundario = V4056,
         rendimento = V403312,
@@ -44,5 +43,4 @@ ler_pnad <- function(ano, trimestre) {
         funcionario_secundario = V4047, carteira_secundario = V4048
       ) %>%
       dplyr::mutate_at(dplyr::vars(ano:estrato), as.integer)
-  resp
 }
